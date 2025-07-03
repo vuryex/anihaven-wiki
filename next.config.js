00ec14b594384997c94c92a42dd7a166fd13fe51
vury-chris <christopherle1223@gmail.com>
2025-07-03 09:05:53 -0400

    fixes for deployment

--- next.config.js
+++ next.config.js
@@ -2,19 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  trailingSlash: true,
   images: {
-    unoptimized: true,
     domains: ['github.com', 'api.github.com', 'raw.githubusercontent.com', 'avatars.githubusercontent.com'],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/admin/:path*',
-        destination: '/admin/index.html',
-      },
-    ];
-  },
+
 };
 
 module.exports = nextConfig;
