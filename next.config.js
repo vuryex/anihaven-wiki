7fe0364667c0a15f7eff6a3dcd87d23641a9e4f0
vury-chris <christopherle1223@gmail.com>
2025-07-03 09:17:18 -0400

    fixes for deployment

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    unoptimized: true,
     domains: ['github.com', 'api.github.com', 'raw.githubusercontent.com', 'avatars.githubusercontent.com'],
   },
-
 };
 
 module.exports = nextConfig;
