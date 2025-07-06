5347690cb5f92ca60a2874def62229e3060db1ba
vury-chris <christopherle1223@gmail.com>
2025-07-06 15:01:59 -0400

    a

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  trailingSlash: true, // Consistent with vercel.json
   images: {
     unoptimized: true,
     domains: ['github.com', 'api.github.com', 'raw.githubusercontent.com', 'avatars.githubusercontent.com'],
